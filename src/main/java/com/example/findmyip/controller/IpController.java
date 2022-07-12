398e5c3fbbd706276ec14438ceaab07162ebe3fd
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-07-12 22:54:20 +0500

    2022-07-12-1

--- src/main/java/com/example/findmyip/controller/IpController.java
+++ src/main/java/com/example/findmyip/controller/IpController.java
@@ -1,23 +1,16 @@
 package com.example.findmyip.controller;
 
-import com.example.findmyip.service.BlackListService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class IpController {
-    private final BlackListService service;
-
-    public IpController(BlackListService service) {
-        this.service = service;
-    }
 
     @GetMapping("/findmyip")
-    public String find(HttpServletRequest request) {
-        String ip = request.getRemoteHost();
-        service.inc(ip);
-        return service.check(ip) ? "You are blocked" : "You are welcome";
+    public Mono<String> find() {
+        return Mono.just("Hello from Jetty");
     }
 }
